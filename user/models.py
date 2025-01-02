550d78b919271bfe54084f631def6e004ed5972b
Chanyeong Lim <dlacksdud2@gmail.com>
2025-01-02 12:58:42 +0900

    add simple user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    token = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'user'
