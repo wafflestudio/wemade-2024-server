34ed7924d3df15244696f76644f809fe07995ba6
Yi Daeun <dawnayi2002@gmail.com>
2025-01-18 22:57:22 +0900

    modify Person, PersonInfo table add Account table

--- person/models.py
+++ person/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 
 class PersonalInfo(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    p_id = models.ForeignKey('Person', on_delete=models.CASCADE)
+    p_info = models.JSONField(null=True, blank=True)  # JSON 데이터를 저장할 필드
 
     class Meta:
         db_table = 'personal_info'
         app_label = 'person'
 
+
 class Person(models.Model):
-    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    p_id = models.IntegerField(null=False, unique=True)  # p_id를 고유값으로 설정
+    name = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'person'
+        app_label = 'person'
+
+
+class Account(models.Model):
+    email = models.EmailField()
+    p_id = models.ForeignKey('Person', null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'account'
         app_label = 'person'
