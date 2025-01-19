f8b7bb2198a944017215c2cfd525e7057caac1fb
Yi Daeun <dawnayi2002@gmail.com>
2025-01-19 16:15:20 +0900

    modify personCard urls

--- personCard/urls.py
+++ personCard/urls.py
@@ -5,7 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('list/', PersonCardListAPI.as_view(), name='person-card-list'),
-    path('search/',  PersonCardSearchListAPIView.as_view(), name='person-card-search'), #query로 받도록
-    path('<int:user_id>/', PersonCardDetailAPI.as_view(), name='person-card-detail'),
-    path('<int:user_id>/update/', PersonCardUpdateDestroyAPI.as_view(), name='person-card-update'),
+    path('list/<int:p_id>', name='person-card-list-detail'),
+    #path('search/',  PersonCardSearchListAPIView.as_view(), name='person-card-search'), #query로 받도록
+    path('<int:p_id>/', PersonCardDetailAPI.as_view(), name='person-card-detail'),
+    path('<int:p_id>/update/', PersonCardUpdateDestroyAPI.as_view(), name='person-card-update'),
 ]
