061c31c0001ccc036b5851eb848ba86269748a6a
Yi Daeun <dawnayi2002@gmail.com>
2025-01-19 15:09:36 +0900

    add person models to the admin

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Person, PersonalInfo, Account
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(PersonalInfo)
+admin.site.register(Account)
