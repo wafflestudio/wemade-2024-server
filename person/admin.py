76226973a43f7885b1522fa20287c1cf36502ae9
Yi Daeun <dawnayi2002@gmail.com>
2025-02-05 01:44:17 +0900

    fix admin - add PersonCardInfo

--- person/admin.py
+++ person/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Person, PersonalInfo
+from .models import *
 
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(PersonalInfo)
+admin.site.register(PersonCardInfo)
 
