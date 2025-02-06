7ad34d31d17b326b83c971774da43138afac7d46
Chanyeong Lim <dlacksdud2@gmail.com>
2025-02-06 19:15:18 +0900

    Revert "test commit for deployment"
    This reverts commit c354c0f929959e202d964bfbef67c0e9266a5adc.

--- wemade_2024_server/settings.py
+++ wemade_2024_server/settings.py
@@ -99,6 +99,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "wemade_2024_server.wsgi.application"
 
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -130,6 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
