b2fbe6429f90eca361b77df833f3aa46c28b2cf2
Yi Daeun <dawnayi2002@gmail.com>
2025-02-13 02:48:45 +0900

    person model 변경사항 반영

--- person/models.py
+++ person/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 
 
-# 본인 수정 가능한 Field: 업무성과, 자격증, 학력
 class PersonCardInfo(models.Model):
     p_card_info = models.JSONField(null=True, blank=True)
 
     class Meta:
         db_table = 'personCard_info'
 
-# 인사팀/최초 1회 입력: 전화번호, 생년월일, 사내 보직 history
+
 class PersonalInfo(models.Model):
-    main_phone_number = models.TextField(max_length=13, null=True)  # '010-0000-0000' 형태, 여러 개일 수 있음
+    main_phone_number = models.TextField(max_length=13, null=True)
     name = models.CharField(max_length=100, null=True)
     emails = models.JSONField()
-    birthday = models.TextField(max_length=10, null=True) #2000-01-01 형태
-    p_info = models.JSONField(null=True, blank=True)  # JSON 데이터를 저장할 필드
+    birthday = models.TextField(max_length=10, null=True)
+    p_info = models.JSONField(null=True, blank=True)
     p_card_info = models.OneToOneField(PersonCardInfo, on_delete=models.CASCADE, null=True)
 
     class Meta:
         db_table = 'personal_info'
 
+
 class Person(models.Model):
-    p_id = models.BigAutoField(primary_key=True) # p_id를 고유값으로 설정
+    p_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     personal_info = models.OneToOneField(PersonalInfo, on_delete=models.SET_NULL, null=True)
-    roles = models.JSONField(null=True)
 
+    # 직접 import 없이 문자열 참조 사용
+    corporations = models.ManyToManyField('company.Corporation', related_name="corporation_persons", blank=True)
+    teams = models.ManyToManyField('company.Team', related_name="team_persons", blank=True)
+
+    roles = models.JSONField(null=True, blank=True)
 
     class Meta:
-        db_table = 'person'
-        app_label = 'person'
+        db_table = "person"
+        app_label = "person"
