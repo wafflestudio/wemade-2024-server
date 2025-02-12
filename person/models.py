3d2797522e5cb5662cc081436071576b6357e2bd
Yi Daeun <dawnayi2002@gmail.com>
2025-02-13 01:28:33 +0900

    company 관련 파트 주석해제

--- person/models.py
+++ person/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from person.models import *
-#from company.models import * #company branch와 병합 후 주석 해제
+from company.models import * #company branch와 병합 후 주석 해제
 
 
 # p_info: 공개정보(자격증 등), p_card_info(비공개/인사카드 정보)
@@ -28,10 +28,10 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     personal_info = models.OneToOneField(PersonalInfo, on_delete=models.SET_NULL, null=True)
 
-    #corporations = models.ManyToManyField(Corporate, related_name="persons", blank=True)
-    #teams = models.ManyToManyField(Team, related_name="persons", blank=True)
+    corporations = models.ManyToManyField(Corporation, related_name="corporation_persons", blank=True)  # 소속 법인
+    teams = models.ManyToManyField(Team, related_name="team_persons", blank=True)  # 소속 팀
 
-    roles = models.JSONField(null=True, blank=True)  # 역할 정보 (예: {"role": "developer", "level": "senior"})
+    roles = models.JSONField(null=True, blank=True)  # 역할 정보
 
     class Meta:
         db_table = "person"
