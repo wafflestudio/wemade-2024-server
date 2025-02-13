60599e32bc6b5c6e818b8756c8d050f7df8790fa
Yi Daeun <dawnayi2002@gmail.com>
2025-02-14 01:59:40 +0900

    fix url description

--- personCard/urls.py
+++ personCard/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     # 사람 이름을 query로 받아 검색하기
     path('search/',  PersonCardSearchListAPIView.as_view(), name='person-card-search'),
 
-    # 인사카드 업데이트
-    path('<int:p_id>/info/update/', PersonalInfoUpdateAPIView.as_view(), name='personal-info-update'),
     # 인사정보 업데이트
+    path('<int:p_id>/info/update/', PersonalInfoUpdateAPIView.as_view(), name='personal-info-update'),
+    # 직무 업데이트 (HR Team)
     path('<int:p_id/roles/update/>', PersonRolesUpdateAPIView.as_view(), name='person-roles-update'),
     # 인사카드 조회
     path('<int:p_id>/', PersonCardDetailAPIView.as_view(), name='person-card-detail'),
