333e04874f7d4eed76bbbee68b9d7ef21a49b17d
Chanyeong Lim <dlacksdud2@gmail.com>
2025-02-20 22:10:46 +0900

    조직도 히스토리 추가

--- company/urls.py
+++ company/urls.py
@@ -5,15 +5,19 @@ from .views import *
 urlpatterns = [
     # Corporation
     path('corp/list/', CorpListAPIView.as_view(), name='corp-list'),  # list all corporations
-    path('corp/<int:c_id>/update/', CorpUpdateDeleteAPIView.as_view(), name='corp-update'),  # update corporation info
-    path('corp/<int:c_id>/delete/', CorpUpdateDeleteAPIView.as_view(), name='corp-delete'),  # delete corporation info
+    #path('corp/<int:c_id>/update/', CorpUpdateDeleteAPIView.as_view(), name='corp-update'),  # update corporation info
+    #path('corp/<int:c_id>/delete/', CorpUpdateDeleteAPIView.as_view(), name='corp-delete'),  # delete corporation info
     path('corp/<int:c_id>/', CorpDetailAPIView.as_view(), name='corp-detail'),  # get specific corporation info
-    path('corp/create/', CorpCreateAPIView.as_view(), name='corp-create'),  # create new corporation
+    #path('corp/create/', CorpCreateAPIView.as_view(), name='corp-create'),  # create new corporation
+    path('corp/commit/', CorpBatchProcessView.as_view(), name='corp-commit'), # use commit for create/delete/update corp
+    path('corp/<int:c_id>/', GetCorpOfCommitView.as_view(), name='corp-of-commit'),
 
     # Team
     path('team/list/', TeamListAPIView.as_view(), name='team-list'),  # list all teams
-    path('team/<int:t_id>/update/', TeamUpdateDeleteAPIView.as_view(), name='team-update'),  # update team info
-    path('team/<int:t_id>/delete/', TeamUpdateDeleteAPIView.as_view(), name='team-delete'),  # delete team info
+    #path('team/<int:t_id>/update/', TeamUpdateDeleteAPIView.as_view(), name='team-update'),  # update team info
+    #path('team/<int:t_id>/delete/', TeamUpdateDeleteAPIView.as_view(), name='team-delete'),  # delete team info
     path('team/<int:t_id>/', TeamDetailAPIView.as_view(), name='team-detail'),  # get specific team info
-    path('team/create/', TeamCreateAPIView.as_view(), name='team-create'),  # create new team
+    #path('team/create/', TeamCreateAPIView.as_view(), name='team-create'),  # create new team
+    path('team/commit/', TeamBatchProcessView.as_view(), name='team-commit'), # use commit for create/delete/update team
+    path('team/<int:t_id>/old/', GetTeamOfCommitView.as_view(), name='team-of-commit'),
 ]
--- company/views.py
+++ company/views.py
@@ -1,10 +1,19 @@
+from django.db.models import Subquery, OuterRef
+from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import ValidationError
 from .serializers import *
 from .paginations import *
 from rest_framework import status
+from django.db.models import OuterRef, Subquery
+import logging
 
+logger = logging.getLogger(__name__)
 
 # Corporation
 class CorpListAPIView(ListAPIView):
@@ -69,7 +78,7 @@ class TeamListAPIView(ListAPIView):
     def get_queryset(self):
         name = self.request.query_params.get('name')
         if not name:
-            return Team.objects.all()
+            return Team.objects.filter(is_active=True)
         return Team.objects.filter(name__icontains=name)
 
 
@@ -112,5 +121,209 @@ class TeamUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        self.perform_destroy(instance)
+        instance.is_active = False
+        instance.save()
         return Response(status=204)
+
+@receiver(post_save, sender=Corporation)
+@receiver(post_save, sender=Team)
+def update_name_history_on_save(sender, instance, **kwargs):
+    if getattr(instance, '_disable_signal', False):
+        return
+
+    if instance.name_history is None:
+        instance.name_history = []
+
+    updated_name_history = instance.name_history + [{
+        'name': instance.name,
+        'commit_id': instance.last_commit.commit_id,
+    }]
+    instance.name_history = updated_name_history
+
+    instance._disable_signal = True
+    try:
+        instance.save(update_fields=["name_history"])
+    finally:
+        instance._disable_signal = False
+
+@receiver(m2m_changed, sender=Team.parent_teams.through)
+def update_parent_team_history_on_m2m_change(sender, instance, action, **kwargs):
+    if action != "post_add":
+        return
+
+    logger.critical(f"Parent team change detected for Team {instance.t_id}: {action}")
+
+    parent_team_id = instance.parent_teams.first().t_id if instance.parent_teams.exists() else ""
+
+    if instance.parent_team_history is None:
+        instance.parent_team_history = []
+
+    updated_parent_history = instance.parent_team_history + [{
+        'parent_id': parent_team_id,
+        'commit_id': instance.last_commit.commit_id,
+    }]
+    instance.parent_team_history = updated_parent_history
+
+    logger.critical(f"Parent team history updated: {updated_parent_history}")
+
+    instance._disable_signal = True
+    try:
+        instance.save(update_fields=["parent_team_history"])
+    finally:
+        instance._disable_signal = False
+
+
+
+class TeamBatchProcessView(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request, *args, **kwargs):
+        if request.data.get('add_commit', True):
+            new_commit = Commit.objects.create(
+                commit_message=request.data.get('commit_message'),
+                commit_author=request.user.person
+            )
+        else:
+            new_commit = Commit.objects.last()
+        new_commit.commit_content.extend(request.data.get('updates', []))
+        new_commit.save()
+
+        results = {"created": [], "updated": [], "deleted": []}
+        errors = []
+
+        for item in request.data.get('updates', []):
+            t_id = item.get('t_id')
+            request_type = item.get('request_type')
+
+            try:
+                if request_type == 'delete':
+                    instance = Team.objects.get(t_id=t_id)
+                    instance.is_active = False
+                    instance.last_commit = new_commit
+                    instance.save()
+                    results["deleted"].append({"t_id": t_id})
+
+                elif request_type == 'update':
+                    instance = Team.objects.get(t_id=t_id)
+                    serializer = TeamUpdateDeleteSerializer(
+                        instance, data=item, partial=True
+                    )
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(last_commit=new_commit)
+                    results["updated"].append(serializer.data)
+
+                elif request_type == 'create':
+                    serializer = TeamCreateSerializer(data=item)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(last_commit=new_commit)
+                    results["created"].append(serializer.data)
+
+                else:
+                    raise ValidationError(f"Invalid request type: {request_type}")
+
+            except Exception as e:
+                errors.append({"item": item, "error": str(e)})
+
+        return Response({"results": results, "errors": errors}, status=200)
+
+
+class CorpBatchProcessView(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request, *args, **kwargs):
+        if request.data.get('add_commit', True):
+            new_commit = Commit.objects.create(
+                commit_message=request.data.get('commit_message'),
+                commit_author=request.user.person
+            )
+        else:
+            new_commit = Commit.objects.last()
+        new_commit.commit_content.extend(request.data.get('updates', []))
+        new_commit.save()
+
+        results = {"created": [], "updated": [], "deleted": []}
+        errors = []
+
+        for item in request.data.get('updates', []):
+            c_id = item.get('c_id')
+            request_type = item.get('request_type')
+
+            try:
+                if request_type == 'delete':
+                    instance = Corporation.objects.get(c_id=c_id)
+                    instance.is_active = False
+                    instance.last_commit = new_commit
+                    instance.save()
+                    results["deleted"].append({"c_id": c_id})
+
+                elif request_type == 'update':
+                    instance = Corporation.objects.get(c_id=c_id)
+                    serializer = CorpUpdateDeleteSerializer(
+                        instance, data=item, partial=True
+                    )
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(last_commit=new_commit)
+                    results["updated"].append(serializer.data)
+
+                elif request_type == 'create':
+                    serializer = CorpCreateSerializer(data=item)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(last_commit=new_commit)
+                    results["created"].append(serializer.data)
+
+                else:
+                    raise ValidationError(f"Invalid request type: {request_type}")
+
+            except Exception as e:
+                errors.append({"item": item, "error": str(e)})
+
+        return Response({"results": results, "errors": errors}, status=200)
+
+class GetCorpOfCommitView(APIView):
+    def get(self, request, *args, **kwargs):
+        target_corp_id = kwargs.get('c_id')
+        target_commit_id = request.query_params.get('commit')
+        teams = Team.objects.filter(corporation__c_id=target_corp_id)
+        children = []
+
+        for sub_team_candidate in teams:
+            for d in sub_team_candidate.parent_team_history[::-1]:
+                if d.get('commit_id') <= int(target_commit_id):
+                    if  d.get('parent_id') == '':
+                        children.append(sub_team_candidate.t_id)
+                    break
+
+        corporation = Corporation.objects.get(c_id=target_corp_id)
+        serializer = CorpDetailSerializer(corporation)
+        response_data = serializer.data.copy()
+
+        for n in corporation.name_history:
+            if n.get('commit_id') <= int(target_commit_id):
+                response_data.update({"name": n.get('name')})
+        response_data.update({"sub_teams": children})
+        return Response(response_data, status=200)
+
+class GetTeamOfCommitView(APIView):
+    def get(self, request, *args, **kwargs):
+        target_team_id = kwargs.get('t_id')
+        target_commit_id = request.query_params.get('commit')
+        teams = Team.objects.all()
+        children = []
+
+        for sub_team_candidate in teams:
+            for d in sub_team_candidate.parent_team_history[::-1]:
+                if d.get('commit_id') <= int(target_commit_id):
+                    if  d.get('parent_id') == int(target_team_id):
+                        children.append(sub_team_candidate.t_id)
+                    break
+
+        team = Team.objects.get(t_id=target_team_id)
+        serializer = TeamDetailSerializer(team)
+        response_data = serializer.data.copy()
+
+        for n in team.name_history:
+            if n.get('commit_id') <= int(target_commit_id):
+                response_data.update({"name": n.get('name')})
+        response_data.update({"sub_teams": children})
+        return Response(response_data, status=200)
+
